52cc9ec0f03c92becfdccaf4dae24643b38c878e
CHOI TAEYOUNG <55708666+92-Theo@users.noreply.github.com>
2024-01-22 15:06:42 +0900

    Initial commit

---
+++ pca10040/s132/ses/Output/Release/Obj/ble_app_uart_pca10040_s132/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/Release/Obj/ble_app_uart_pca10040_s132/ses_startup_nrf_common.o: \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\ses_startup_nrf_common.s
---
+++ pca10040/s132/ses/Output/Release/Obj/ble_app_uart_pca10040_s132/system_nrf52.d
@@ -0,0 +1,24 @@
+Output/Release/Obj/ble_app_uart_pca10040_s132/system_nrf52.o: \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\system_nrf52.c \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stdint.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stdbool.h \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf.h \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\system_nrf52.h \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf52_bitfields.h \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf51_to_nrf52.h \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf52_name_change.h \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\compiler_abstraction.h \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf_peripherals.h \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf52832_peripherals.h \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf_erratas.h \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf51_erratas.h \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf52_erratas.h \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf53_erratas.h \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf91_erratas.h \
+ D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\system_nrf52_approtect.h
