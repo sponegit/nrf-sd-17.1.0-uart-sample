6f1265a470a21782f99458fdceaf4e5bd3025454
CHOI TAEYOUNG <55708666+92-Theo@users.noreply.github.com>
2024-06-25 17:13:21 +0900

    clean code

--- pca10040/s132/ses/Output/Release/Obj/ble_app_uart_pca10040_s132/SEGGER_RTT_Syscalls_SES.d
+++
@@ -1,3 +0,0 @@
-Output/Release/Obj/ble_app_uart_pca10040_s132/SEGGER_RTT_Syscalls_SES.o: \
- D\:\workplace\nrf_sd\sd_17.1.0\src\nRF5_SDK_17.1.0_ddde560\external\segger_rtt\SEGGER_RTT_Syscalls_SES.c \
- ../config/sdk_config.h
